585991dbe1ab4f28a3cace53c55e708831e02e5c
cozy-hn <jhk2721@gmail.com>
2024-07-21 06:24:05 +0900

    feat: basic setting shell script

---
+++ set-server.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+ENV_DIR="myenv"
+
+if [ ! -d "$ENV_DIR" ]; then
+    python3 -m venv $ENV_DIR
+    echo "Virtual environment created at $ENV_DIR."
+else
+    echo "Virtual environment already exists at $ENV_DIR."
+fi
+
+source $ENV_DIR/bin/activate
+echo "Virtual environment activated."
+
+if [ -f "requirements.txt" ]; then
+    pip install -r requirements.txt
+    echo "Installed packages from requirements.txt."
+else
+    echo "requirements.txt not found."
+fi
+
+deactivate
+echo "Virtual environment deactivated."
