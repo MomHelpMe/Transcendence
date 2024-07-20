d140c0092ba8e2303b9b4933e55d72105d5bdca7
cozy-hn <jhk2721@gmail.com>
2024-07-21 06:30:30 +0900

    fix: run-server.sh add migrate

--- run-server.sh
+++ run-server.sh
@@ -1,2 +1,8 @@
 source myenv/bin/activate
+echo "Virtual environment activated."
+
+echo "Applying migrations..."
+python manage.py migrate
+
+echo "Starting development server..."
 python manage.py runserver
