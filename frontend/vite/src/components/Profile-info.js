b4eec6d746b743d83388fd3445993c9593fbe8bf
seonjo1 <tjdgus9684@gmail.com>
2024-08-07 12:48:06 +0900

    fix: 화면 크기에 따른 변화 수정

--- frontend/vite/src/components/Profile-info.js
+++ frontend/vite/src/components/Profile-info.js
@@ -9,18 +9,51 @@ export class ProfileInfo extends Component {
 
   template () {
 	this.user = { nickname: "seonjo", img_url: "../../소년명수.png"};
+	this.win = 35;
+	this.lose = 20;
+	this.rate = 70;
 	console.log(this.props.nickname, this.user);
     return `
-		<div id="profile-left">
-			<div id="profile-edit">
-				${this.props.nickname === this.user.nickname ? `<div id="profile-edit-button">edit</div>` : ""}
+		<div id="profileBox">
+			<img src="../../back.png" id="goBack"></img>
+			<div id="profile">
+				<div id="profile-left">
+					<div id="profileHeaderBox">
+						<span id="profileHeader">Profile</span>
+					</div>
+					<div id="userInfo">
+						<div id="profile-edit">
+							${this.props.nickname === this.user.nickname ? `<div id="profile-edit-button">edit</div>` : ""}
+						</div>
+						<div id="profileUserName">${this.user.nickname}
+						</div>
+						<div id="profileImgBox">
+							${this.user.img_url === "" ?
+								`<img id="profileImg" src="../../friends.png"></img>` :
+								`<img id="profileImg" src="${this.user.img_url}"></img>`}
+						</div>
+					</div>
+				</div>
+				<div id="profile-right">
+					<div id="match">
+						<div id="matchInfo">
+							<span id="win">
+								Win  ${this.win}
+							</span>
+							<span id="lose">
+								Lose  ${this.lose}
+							</span>
+							<span id="rate">
+								Rate  ${this.rate}%
+							</span>
+						</div>
+						<div id="matchHistory">
+							<span id="matchHistory>
+							</span>
+						</div>
+					</div>
+				</div>
 			</div>
-			<div id="profileUserName">${this.user.nickname}
-			</div>
-			<div id="profileImg>
-			</div>
-		</div>
-		<div id="profile-right>
 		</div>
     `;
   }
