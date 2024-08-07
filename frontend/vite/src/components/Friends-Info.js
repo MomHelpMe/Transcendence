745db6359da0de9dd2d786c055ca73585deebe59
seonjo1 <tjdgus9684@gmail.com>
2024-08-07 15:39:19 +0900

    fix: 필요없는 거 제거

--- frontend/vite/src/components/Friends-Info.js
+++ frontend/vite/src/components/Friends-Info.js
@@ -3,14 +3,10 @@ export class FriendsInfo extends Component {
 
   template () {
 	const { is_online, nickname, img_url } = this.props;
-    let nick = nickname;
-	if (nick.length > 10) {
-      nick = nick.substring(0, 10) + "...";
-    }
     return `
 		<div id="onlineInfo">
 			${is_online ? `<div id="online"></div>` : `<div id="offline"></div>`}
-			<div id="friendNick">${nick}</div>
+			<div id="friendNick">${nickname}</div>
 		</div>
 		<div id="friendImage">
 			<img id="friendImage" src="../../소년명수.png"></img>
--- frontend/vite/src/components/List.js
+++ frontend/vite/src/components/List.js
@@ -6,11 +6,7 @@ export class List extends Component {
 		const { className, contents } = this.props;
 		return `
 		  ${contents.map(element => {
-			let text = element;
-			if (text.length > 10) {
-				text = text.substring(0, 10) + "...";
-			  }
-			return `<li class="${className}" id="${element}">${text}</li>`}).join('')}
+			return `<li class="${className}" id="${element}">${element}</li>`}).join('')}
 		`;
 	}
 }
