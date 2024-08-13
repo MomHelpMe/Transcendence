ea55db85388d804e1039cc0fc90fbacb967674d9
seonjo1 <tjdgus9684@gmail.com>
2024-08-13 16:17:59 +0900

    feat: jwt 토큰 디코딩함수 추가

--- frontend/src/components/Main-Menu.js
+++ frontend/src/components/Main-Menu.js
@@ -1,24 +1,14 @@
 import { Component } from "../core/Component.js";
 import { List } from "./List.js";
 import { changeUrl } from "../core/router.js";
+import { parseJWT } from "../core/jwt.js"
 
 export class Menu extends Component {
 
 	template () {
-		fetch(url, {
-			method: 'GET',
-			credentials: 'include', // 쿠키를 포함하여 요청 (사용자 인증 필요 시)
-		})
-		.then(response => {
-			if (!response.ok) {
-				throw new Error('Network response was not ok');
-			}
-			return response.json();
-		})
-		.then(data => {
-			this.userNick = data.userNick;
-		})
-		.catch(error => console.error('Fetch error:', error));
+		const payload = parseJWT();
+		if (!payload) this.uid = null;
+		else this.uid = payload.uid;
 
 		return `
 			<div id="menuBox">
@@ -39,7 +29,8 @@ export class Menu extends Component {
 		});
 
 		this.addEvent('click', '#Profile', () => {
-			changeUrl(`/main/profile/${this.userNick}`);
+			if (this.uid) changeUrl(`/main/profile/${this.uid}`);
+			else changeUrl("/");
 		});
 
 		this.addEvent('click', '#Logout', () => {
