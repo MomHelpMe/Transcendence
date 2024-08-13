7b2b93f4c2d757b8c0232fea834b4bc89e9ade0b
seonjo1 <tjdgus9684@gmail.com>
2024-08-13 13:49:12 +0900

    feat: main-menu에 api 형식 구현

--- frontend/src/components/Main-Menu.js
+++ frontend/src/components/Main-Menu.js
@@ -4,29 +4,56 @@ import { changeUrl } from "../core/router.js";
 
 export class Menu extends Component {
 
-  template () {
-    return `
-		<div id="menuBox">
-		  <ul id="gameMenu"></ul>
-		  <ul id="userMenu"></ul>
-		</div>
-    `;
-  }
+	template () {
+		fetch(url, {
+			method: 'GET',
+			credentials: 'include', // 쿠키를 포함하여 요청 (사용자 인증 필요 시)
+		})
+		.then(response => {
+			if (!response.ok) {
+				throw new Error('Network response was not ok');
+			}
+			return response.json();
+		})
+		.then(data => {
+			this.userNick = data.userNick;
+		})
+		.catch(error => console.error('Fetch error:', error));
 
-  mounted(){
-	this.children.push(new List(document.querySelector("ul#gameMenu"), { className: "gameMode", contents: ["Local Game", "Multi Game", "AI", "Tournament"]}));
-	this.children.push(new List(document.querySelector("ul#userMenu"), { className: "showInfo", contents: ["Friends", "Profile", "Setting"]}));
-  }
+		return `
+			<div id="menuBox">
+			<ul id="gameMenu"></ul>
+			<ul id="userMenu"></ul>
+			</div>
+		`;
+	}
 
-  setEvent () {
-    this.addEvent('click', '#Friends', () => {
-      changeUrl("/main/friends");
-    });
+	mounted(){
+		this.children.push(new List(document.querySelector("ul#gameMenu"), { className: "gameMode", contents: ["Local Game", "Multi Game", "AI", "Tournament"]}));
+		this.children.push(new List(document.querySelector("ul#userMenu"), { className: "showInfo", contents: ["Friends", "Profile", "Logout"]}));
+	}
 
-	this.addEvent('click', '#Profile', () => {
-		// api로 user 정보 가져오기
-		const user = "seonjo";
-	  changeUrl(`/main/profile/${user}`);
-	});
-  }
+	setEvent () {
+		this.addEvent('click', '#Friends', () => {
+			changeUrl("/main/friends");
+		});
+
+		this.addEvent('click', '#Profile', () => {
+			changeUrl(`/main/profile/${this.userNick}`);
+		});
+
+		this.addEvent('click', '#Logout', () => {
+			fetch(url, {
+				method: 'DELETE',
+				credentials: 'include', // 쿠키를 포함하여 요청 (사용자 인증 필요 시)
+			})
+			.then(response => {
+				if (!response.ok) {
+					throw new Error('Network response was not ok');
+				}
+				changeUrl(`/`);
+			})
+			.catch(error => console.error('Fetch error:', error));
+		});
+	}
 }
