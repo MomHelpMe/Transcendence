596da1fbf3cedc20f99b74655e0ad5a014fe26b8
michang <minseok128128@gmail.com>
2024-08-25 18:38:32 +0900

    fix : 게임이 그려지지 않는 버그 수정

--- frontend/src/components/Game-Local.js
+++ frontend/src/components/Game-Local.js
@@ -2,6 +2,6 @@ import { GameDefault } from "./Game-Default.js";
 import { GameCore } from "./Game-Core.js";
 export class GameLocal extends GameDefault {
 	mounted(){
-		this.children.push(new GameCore(document.querySelector("div#game"), this.props));
+		new GameCore(document.querySelector("div#game"), this.props);
 	}
 }
