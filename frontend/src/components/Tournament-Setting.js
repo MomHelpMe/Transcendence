c86211088c3d9317830a6fe5b56691c189b39517
seonjo1 <tjdgus9684@gmail.com>
2024-08-27 14:41:35 +0900

    fix: img css 디자인 수정

--- frontend/src/components/Tournament-Setting.js
+++ frontend/src/components/Tournament-Setting.js
@@ -4,6 +4,16 @@ import { changeUrl } from "../core/router.js";
 export class TournamentSetting extends Component {
 
 	template () {
+		// fetch(){
+		//	tournament history 조회
+		//}
+
+		let idx = 0;
+		const max = 4;
+		const games = {
+			"game_info": '{ "game1": {"Jiko": 1, "PSY": 2}, "game2": {"KimYuna": 2, "BTS": 1}, "game3": {"PSY": 2, "KimYuna": 1} }'
+		};
+
 		return `
 			<div id="tournament-box">
 				<div id="tournament-game-menu">Game</div>
@@ -39,9 +49,38 @@ export class TournamentSetting extends Component {
 						<input class="tournament-input" autocomplete="off" id="tournament-nick3" maxlength="8" placeholder="nickname3"></input>
 						<input class="tournament-input" autocomplete="off" id="tournament-nick4" maxlength="8" placeholder="nickname4"></input>
 					</div>
+					<div id="tournament-start-button">S T A R T</div>
 				</div>
-				<div id="tournament-start-button">S T A R T</div>
 				<div id="tournament-history-body">
+					<div id="tournament-crown-box">
+						<img id="crown" src="/img/crown.png"></img>
+					</div>
+					<div id="tournament-players">
+						<div id="tournament-nick">Winner</div>
+					</div>
+					<div id="tournament-lines">
+						<div id="tournament-line1"></div>
+					</div>
+					<div id="tournament-players">
+						<div id="tournament-nick">TBD</div>
+						<div id="tournament-nick">TBD</div>
+					</div>
+					<div id="tournament-lines">
+						<div id="tournament-line2"></div>
+						<div id="tournament-line2"></div>
+					</div>
+					<div id="tournament-players">
+						<div id="tournament-nick2">TBD</div>
+						<div id="tournament-nick2">TBD</div>
+						<div id="tournament-blank"></div>
+						<div id="tournament-nick2">TBD</div>
+						<div id="tournament-nick2">TBD</div>
+					</div>
+					<div id="tournament-history-button-box">
+						<div id="tournament-prev-button"></div>
+						<div id="tournament-game-name">Game 1</div>
+						<div id="tournament-next-button"></div>
+					</div>
 				</div>
 			</div>
 		`;
@@ -62,14 +101,12 @@ export class TournamentSetting extends Component {
 			const mainBody = document.querySelector('#tournament-main-body');
 			const gameBody = document.querySelector('#tournament-game-body');
 			const historyBody = document.querySelector('#tournament-history-body');
-			const startButton = document.querySelector("#tournament-start-button");
 
 			gameMenu.style.color = '#6dd5fa';
 			historyMenu.style.color = 'white';
 			mainBody.style.display = 'none';
 			historyBody.style.display = 'none';
 			gameBody.style.display = 'flex';
-			startButton.style.display = 'flex';
 		});
 		
 		this.addEvent('click', '#tournament-history-menu', (event) => {
@@ -78,14 +115,12 @@ export class TournamentSetting extends Component {
 			const mainBody = document.querySelector('#tournament-main-body');
 			const gameBody = document.querySelector('#tournament-game-body');
 			const historyBody = document.querySelector('#tournament-history-body');
-			const startButton = document.querySelector("#tournament-start-button");
 			
 			historyMenu.style.color = '#6dd5fa';
 			gameMenu.style.color = 'white';
 			mainBody.style.display = 'none';
 			gameBody.style.display = 'none';
 			historyBody.style.display = 'flex';
-			startButton.style.display = 'none';
 		});
 	}
 }
