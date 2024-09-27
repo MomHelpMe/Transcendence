591d56a5e35a323f5ffc992f0879698b64015ed7
seonjo1 <tjdgus9684@gmail.com>
2024-09-27 16:09:46 +0900

    feat: game에 따른 nick1, nick2, game 저장

--- frontend/src/components/Game-Tournament.js
+++ frontend/src/components/Game-Tournament.js
@@ -4,26 +4,28 @@ import { changeUrl } from "../core/router.js";
 
 export class GameTournament extends GameDefault {
 	mounted(){
-		const nicknames = JSON.parse(localStorage.getItem("nickanems"));
+		const nicknames = JSON.parse(localStorage.getItem("nicknames"));
+		console.log(nicknames[0]);
 		if (!localStorage.getItem('game1')) {
-			this.props[game] = 1;
-			this.props[nick1] = nicknames[0];
-			this.props[nick2] = nicknames[1];
+			this.props["game"] = 1;
+			this.props["nick1"] = nicknames["nick1"];
+			this.props["nick2"] = nicknames["nick2"];
 		} else if (!localStorage.getItem('game2')) {
-			this.props[game] = 2;
-			this.props[nick1] = nicknames[2];
-			this.props[nick2] = nicknames[3];
+			this.props["game"] = 2;
+			this.props["nick1"] = nicknames["nick3"];
+			this.props["nick2"] = nicknames["nick4"];
 		} else if (!localStorage.getItem('game3')) {
-			this.props[game] = 3;
+			this.props["game"] = 3;
 			const game1 = JSON.parse(localStorage.getItem('game1'));
 			const game2 = JSON.parse(localStorage.getItem('game2'));
-			for (i = 0; i < 4; i++) {
-				if (game1.winner === nicknames[i]) this.props[nick1] = nicknames[i];
-				else if (game2.winner === nicknames[i]) this.props[nick2] = nicknames[i];
-			}
+			nicknames.forEach((nickname) => {
+				if (game1.winner === nickname) this.props["nick1"] = nickname;
+				if (game2.winner === nickname) this.props["nick2"] = nickname;
+			});
 		} else {
 			changeUrl("/main/tournament");
 		}
+		console.log(this.props);
 		new GameTournamentCore(document.querySelector("div#game"), this.props);
 	}
 }
